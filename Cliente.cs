0d5fbbde9a28d42e78306739c337e8e2143f4de4
JocielPassos <jociel.passos@aluno.senai.br>
2023-05-17 16:21:15 -0300

    Construtores

---
+++ Cliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoAObjetos.Dia3
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set;}
+
+        public Cliente()
+        { 
+        
+        }
+        public Cliente(int id, string cpf, string nome) 
+        {
+            Id = id;
+            Nome = cpf; 
+            Cpf = nome;
+        }
+
+
+        public Cliente(int id)
+        {
+            Id = id;
+        }
+
+        public Cliente(string cpf, string nome)
+        {
+            Cpf = cpf;
+            Nome = nome;
+        }
+
+       
+        //Exercicio 
+        //Cire construtores
+        //Sobrecaregue
+
+    }
+}
